23d192a612ac57c2c819e0f4b56153a4b49e1c39
Kanhaiya-verma <Kanhaiyaverma2580@gmail.com>
2022-08-08 17:58:58 +0530

    comment removed

--- update.php
+++ update.php
@@ -7,11 +7,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $AGE = $_POST['dAge'];
     $EMAIL = $_POST['dMail'];
     $PASSWORD = $_POST['dPassword'];
-    // echo "$ID <br>";
-    // echo "$NAME <br>";
-    // echo "$AGE <br>";
-    // echo "$EMAIL <br>";
-    // echo "$PASSWORD <br>";
+    
 
     $sql = "UPDATE details SET dname='$NAME' ,dAge='$AGE', dMail='$EMAIL', dPassword='$PASSWORD' WHERE dId= '$ID'";
     $result = mysqli_query($conn, $sql);
